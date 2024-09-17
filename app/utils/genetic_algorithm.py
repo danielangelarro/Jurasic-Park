275546495f2290d78b2c4a8ee6a460f3751cbbde
Daniel Ángel <danielangelarro@gmail.com>
2024-09-16 21:37:11 -0400

    update genetic algorithm

--- app/utils/genetic_algorithm.py
+++ app/utils/genetic_algorithm.py
@@ -1,29 +1,10 @@
 import numpy as np
-
 from app.utils.simulacion import Simulacion
 
-
 class CombinacionGenetica:
-    def __init__(
-            self,
-            genotipo={},
-            tamano_entorno = 10,
-            n_humanos = 10,
-            n_ciclos = 100,
-            reproduccion = True,
-            dinosaurios = [],
-            entorno = None,
-    ):
-
+    def __init__(self, genotipo={}, tamano_entorno=10, n_humanos=10, n_ciclos=100, reproduccion=True, dinosaurios=[], entorno=None):
         if genotipo == {}:
-            genotipo = {
-                'fuerza': np.random.randint(5, 100),
-                'velocidad': np.random.randint(5, 100),
-                'resistencia': np.random.randint(5, 100),
-                'inteligencia': np.random.randint(5, 100),
-                'adaptabilidad': np.random.randint(5, 100),
-                'supervivencia': np.random.randint(5, 100),
-            }
+            genotipo = self.generar_genotipo_aleatorio()
 
         self.simulacion = Simulacion(
             tamano_entorno=tamano_entorno,
@@ -34,27 +15,40 @@ class CombinacionGenetica:
             dinosaurios=dinosaurios,
             entorno=entorno,
             save_results=True
-        )        
+        )
         self.genotipo = genotipo.copy()
         self.resultado = self.simulacion.ejecutar()
         self.aptitud = self.resultado["duracion_supervivencia"]
         self.max_poblacion = self.resultado["poblacion_total"]
+
+    def generar_genotipo_aleatorio(self):
+        return {
+            'fuerza': np.random.randint(5, 100),
+            'velocidad': np.random.randint(5, 100),
+            'resistencia': np.random.randint(5, 100),
+            'inteligencia': np.random.randint(5, 100),
+            'adaptabilidad': np.random.randint(5, 100),
+            'supervivencia': np.random.randint(5, 100)
+        }
 
     def __str__(self):
         return f"({self.genotipo})"
 
 
 class GeneticAlgorithm:
-    def __init__(self,
-                 tamano_entorno=10,
-                 n_humanos=10,
-                 n_ciclos=100,
-                 n_generaciones=500,
-                 n_poblacion=50,
-                 reproduccion=True,
-                 dinosaurios=[],
-                 entorno=None,
-                 ):
+    def __init__(
+            self, 
+            tamano_entorno=10, 
+            n_humanos=10, 
+            n_ciclos=100, 
+            n_generaciones=500, 
+            n_poblacion=50, 
+            reproduccion=True, 
+            dinosaurios=[], 
+            entorno=None, 
+            tasa_mutacion_inicial=0.1, 
+            elitismo=0.05
+        ):
         self.entorno = entorno
         self.tamano_entorno = tamano_entorno
         self.n_humanos = n_humanos
@@ -63,7 +57,8 @@ class GeneticAlgorithm:
         self.n_poblacion = n_poblacion
         self.reproduccion = reproduccion
         self.dinosaurios = dinosaurios
-
+        self.tasa_mutacion_inicial = tasa_mutacion_inicial
+        self.elitismo = elitismo
         self.poblacion = self.crear_poblacion()
 
     def crear_combinacion_genetica(self, genotipo={}):
@@ -84,61 +79,45 @@ class GeneticAlgorithm:
         self.poblacion.sort(key=lambda x: (x.aptitud, x.max_poblacion), reverse=True)
 
     def crossover(self, padre: dict, madre: dict):
-        genotipo_hijo1 = {
-            "fuerza": 0,
-            "velocidad": 0,
-            "resistencia": 0,
-            "inteligencia": 0,
-            "adaptabilidad": 0,
-            "supervivencia": 0
-        }
+        # Utilizando crossover de un solo punto
+        punto_corte = np.random.randint(1, len(padre))
+        hijo1 = {**padre}
+        hijo2 = {**madre}
+        for key in list(padre.keys())[punto_corte:]:
+            hijo1[key], hijo2[key] = madre[key], padre[key]
+        return hijo1, hijo2
 
-        genotipo_hijo2 = {
-            "fuerza": 0,
-            "velocidad": 0,
-            "resistencia": 0,
-            "inteligencia": 0,
-            "adaptabilidad": 0,
-            "supervivencia": 0
-        }
-
-        for key in padre.keys():
-            if np.random.rand() > 0.5:
-                genotipo_hijo1[key] = padre[key]
-                genotipo_hijo2[key] = madre[key]
-            else:
-                genotipo_hijo1[key] = madre[key]
-                genotipo_hijo2[key] = padre[key]
-
-        return genotipo_hijo1, genotipo_hijo2
-
-    def mutacion(self, genotipo: dict, tasa_mutacion=0.1):
+    def mutacion(self, genotipo: dict, tasa_mutacion_actual):
+        # Mutación con tasa adaptativa
         for key in genotipo.keys():
-            if np.random.rand() < tasa_mutacion:
-                genotipo[key] += np.random.normal(loc=0, scale=1)
-                genotipo[key] = max(10, min(100, genotipo[key]))
-
+            if np.random.rand() < tasa_mutacion_actual:
+                genotipo[key] += np.random.normal(loc=0, scale=5)
+                genotipo[key] = max(5, min(100, genotipo[key]))
         return genotipo
 
     def evolucionar(self):
         historial_poblacion = []
         
-        for _ in range(self.n_generaciones):
+        for generacion in range(self.n_generaciones):
             self.seleccion()
+            elite_count = int(self.elitismo * len(self.poblacion))
+            nueva_poblacion = self.poblacion[:elite_count]  # Preservamos a los mejores individuos (elitismo)
 
-            historial_poblacion.append(self.poblacion[0].aptitud)
-
-            nueva_poblacion = []
-            for i in range(0, len(self.poblacion), 2):
-                padre, madre = self.poblacion[i], self.poblacion[i + 1]
+            # Adaptación de tasa de mutación para mantener diversidad
+            tasa_mutacion_actual = self.tasa_mutacion_inicial * (1 - generacion / self.n_generaciones)
+            
+            while len(nueva_poblacion) < self.n_poblacion:
+                # Selección de padres aleatoria (torneo)
+                padre, madre = np.random.choice(self.poblacion[:10], 2)
                 hijo1, hijo2 = self.crossover(padre.genotipo, madre.genotipo)
-                self.mutacion(hijo1)
-                self.mutacion(hijo2)
+                hijo1 = self.mutacion(hijo1, tasa_mutacion_actual)
+                hijo2 = self.mutacion(hijo2, tasa_mutacion_actual)
                 nueva_poblacion.extend([
                     self.crear_combinacion_genetica(hijo1),
                     self.crear_combinacion_genetica(hijo2)
                 ])
 
             self.poblacion = nueva_poblacion.copy()
-        
+            historial_poblacion.append(self.poblacion[0].aptitud)
+
         return historial_poblacion
